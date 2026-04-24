94680c19099a0bc9979eadcf79e5caf59a7f2b18
Mekmun Sopheaktra <Mekmunsopheaktra@gmail.com>
2026-04-24 23:33:24 +0700

    update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,6 +8,10 @@ module.exports = {
             instances: 1,
             autorestart: true,
             watch: false,
+            out_file: "./logs/out.log",
+            error_file: "./logs/error.log",
+            log_file: "./logs/combined.log",
+            time: true,
             env: {
                 NODE_ENV: "development"
             }
